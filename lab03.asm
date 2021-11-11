240d6771f21311a63dd36b583dc7c2f348c3bdc5
nataliaanto <92333442+nataliaanto@users.noreply.github.com>
2021-11-11 18:13:54 +0200

    Update lab03.asm

--- lab03.asm
+++ lab03.asm
@@ -10,6 +10,8 @@
 .data
 anna:  .asciiz "anna"
 bobob: .asciiz "bobob"
+input: .asciiz
+input_len: .word
 
 ###############################################################################
 .text
@@ -29,15 +31,38 @@ bobob: .asciiz "bobob"
 
 
 pdrome:
-###############################################################################
-# Write you code here.
-# Any code above the label swapArray is not executed by the tester! 
-###############################################################################
-   
+la	$a0, input_len	# Load data
+	lw	$a0, 0($a0)
+	la	$a1, input
+	jal 	isPalindrome	# Do the Palindrome check
+	add	$a0, $v0, $zero
+	jal	pdrome	# Print
+	addi	$v0, $zero, 10
+	syscall			# Exit
+
+
+isPalindrome:
+	# Check base case
+	slti	$t0, $a0, 2
+	bne	$t0, $zero, returnTrue
+
+	# Make sure first and last are equal
+	lb	$t0, 0($a1)
+	addi	$t1, $a0, -1
+	add	$t1, $t1, $a1
+	lb	$t1, 0($t1)
+	bne	$t0, $t1, returnFalse
 	
+	# Shift pointer, length, recurse
+	addi	$a0, $a0, -2
+	addi	$a1, $a1, 1
+	j	isPalindrome
+	
+returnFalse:
+	addi	$v0, $zero, 0
+	jr	$ra
 
-###############################################################################
-# End of your code.
-###############################################################################
-  jr $ra
 
+returnTrue:
+	addi	$v0, $zero, 1
+	jr	$ra
